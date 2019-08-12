05fc4016fb4430a63b5317ef74fab8ab6bbdaf74
kaaronayy <53946724+kaaronayy@users.noreply.github.com>
2019-08-12 23:30:13 +0100

    Add files via upload

---
+++ real-time-server/server/server.js
@@ -0,0 +1,48 @@
+const http = require("http");
+
+http
+  .createServer((request, response) => {
+    console.log("Requested url: " + request.url);
+
+    request.on("close", () => {
+        if (!response.finished) {
+          response.end();
+          console.log("Stopped sending events.");
+        }
+      });
+
+    if (request.url.toLowerCase() === "/events") {
+      response.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*"
+      });
+
+      setTimeout(() => {
+        if (!response.finished) {
+          response.write("event: flightStateUpdate\n");
+          response.write('data: {"flight": "I768", "state": "landing"}');
+          response.write("\n\n");
+        }
+      }, 3000);
+
+      setTimeout(() => {
+        response.write("event: flightStateUpdate\n");
+        response.write('data: {"flight": "I768", "state": "landed"}');
+        response.write("\n\n");
+      }, 6000);
+
+      setTimeout(() => {
+        response.write("event: flightRemoval\n");
+        response.write('data: {"flight": "I768"}');
+        response.write("\n\n");
+      }, 9000);
+    } else {
+      response.writeHead(404);
+      response.end();
+    }
+  })
+  .listen(5000, () => {
+    console.log("Server running at http://127.0.0.1:5000/");
+  });
